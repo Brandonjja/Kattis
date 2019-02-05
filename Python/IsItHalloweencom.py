d587c199732948f8af97c1ac84adc3bff1890e35
Brandonjja <brandonjja@hotmail.com>
2019-02-05 17:04:40 -0400

    Create IsItHalloweencom.py

---
+++ Python/IsItHalloweencom.py
@@ -0,0 +1,10 @@
+# IsItHalloween.com
+
+data = input().split()
+
+if data[0] == "OCT" and data[1] == "31":
+    print("yup")
+elif data[0] == "DEC" and data[1] == "25":
+    print("yup")
+else:
+    print("nope")
